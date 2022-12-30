b0b6053f03486aeb59623581df622f1f98dc3a13
Himanshu Prajapati <ipshackathon2022TD@gmail.com>
2022-12-31 00:58:36 +0530

    deployee

--- index.js
+++ index.js
@@ -52,41 +52,44 @@ app.use(express.json());
 
 app.use("/api", router);
 
-const httpServer = http.createServer(app);
+app.listen(port,()=>{ console.log(`Server running at ${port}`);
+})
+
+// const httpServer = http.createServer(app);
 
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-httpServer.listen(port);
-httpServer.on("error", onError);
-httpServer.on("listening", onListening);
+// /**
+//  * Listen on provided port, on all network interfaces.
+//  */
+// httpServer.listen(port);
+// httpServer.on("error", onError);
+// httpServer.on("listening", onListening);
 
-function onError(error) {
-  if (error.syscall !== "listen") throw error;
+// function onError(error) {
+//   if (error.syscall !== "listen") throw error;
 
-  let bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+//   let bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use of  Data");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
+//   // handle specific listen errors with friendly messages
+//   switch (error.code) {
+//     case "EACCES":
+//       console.error(bind + " requires elevated privileges");
+//       process.exit(1);
+//       break;
+//     case "EADDRINUSE":
+//       console.error(bind + " is already in use of  Data");
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
-function onListening() {
-  let addr = httpServer.address();
-  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
-  console.log(`Listening on ${bind}`);
-}
+// /**
+//  * Event listener for HTTP server "listening" event.
+//  */
+// function onListening() {
+//   let addr = httpServer.address();
+//   const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+//   console.log(`Listening on ${bind}`);
+// }
